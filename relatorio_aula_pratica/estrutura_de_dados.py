d2ddcb38d21be8f751724ab0b6e51f0d5ca96bb4
JulioCEstudos <julio.tati.oliver@gmail.com>
2024-09-17 22:22:54 -0300

    estrutura da atividade

---
+++ relatorio_aula_pratica/estrutura_de_dados.py
@@ -0,0 +1,16 @@
+class Node:
+ def __init__(self, data):
+
+
+class LinkedList:
+ def __init__(self):
+ # Insira aqui seu codigo….
+ def append(self, data):
+ # Insira aqui seu codigo….
+ def print_list(self):
+ # Insira aqui seu codigo…..
+# Implementar uma função para contar o número de nós em uma lista encadeada.
+def count_nodes(linked_list):
+ # Insira aqui seu codigo
+# REALIZAR a implementação do exercício
+# IMPRIMIR O NUMERO DE NOS.....
